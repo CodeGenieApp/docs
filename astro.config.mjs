ea2eda08ea424addb151d3f70c9d60fc185da287
Brett <brett.j.andrews@gmail.com>
2023-11-19 14:40:52 +1000

    setup github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://docs.codegenie.codes',
 	integrations: [
 		starlight({
 			title: 'Documentation',
 			social: {
-				// github: 'https://github.com/codegenie',
+				github: 'https://github.com/CodeGenieApp',
 			},
 			sidebar: [
 				{
